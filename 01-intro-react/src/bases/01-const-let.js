d36e6c37f44656ed4128af108febf450e045491d
thePanaMiguel09 <miguichavezbarreram@gmail.com>
2025-07-14 10:37:16 -0500

    Template String

---
+++ 01-intro-react/src/bases/01-const-let.js
@@ -0,0 +1,12 @@
+const name = "Miguel Ángel";
+const surName= "Chavez";
+
+
+console.log(name, surName);
+
+
+if(true) {
+    //Este es un scope, es decir, name solo existe dentro del condicional
+    let name = 'Angela';
+    console.log(name);
+}
