34c182a25c978c7a5bbe546dabceda23b420e04d
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-12-01 22:23:15 -0500

    CSSProperties styles

---
+++ first-steps/src/MyAwesomeApp.tsx
@@ -0,0 +1,33 @@
+import type { CSSProperties } from "react";
+
+const titleStyles: CSSProperties = {
+  backgroundColor: "red",
+  color: "white",
+  padding: 5,
+  borderRadius: 20,
+  display: "flex",
+  justifyContent: "center",
+};
+
+export function MyAwesomeApp() {
+  const name = "Miguel";
+  const lastName = "Chávez";
+
+  const favoriteGames = ["Pubg Mobile", "Fornite", "Metal"];
+  const isActive = true;
+
+  const address = {
+    zipCode: "Crra 29A",
+    city: "Florencia",
+  };
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{lastName}</h2>
+      <p>{favoriteGames.join(", ")}</p>
+      <h1>{isActive ? "Activo" : "Inactivo"}</h1>
+      <h1 style={titleStyles}>{JSON.stringify(address)}</h1>
+    </div>
+  );
+}
