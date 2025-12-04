ccc4a421cb400256f4e4451e86494e741d7d2710
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-12-03 23:46:14 -0500

    ItemCounter style test implemented

--- first-steps/src/shopping-cart/ItemCounter.test.tsx
+++ first-steps/src/shopping-cart/ItemCounter.test.tsx
@@ -20,39 +20,56 @@ describe("ItemCounter", () => {
     expect(screen.getByText(quanity)).toBeDefined();
   });
 
-  test('shoul increase +1 button pressed', () => {
-    render(<ItemCounter name="Test Item" quantity={1}/>);
+  test("shoul increase +1 button pressed", () => {
+    render(<ItemCounter name="Test Item" quantity={1} />);
 
-    const [, buttonAdd] = screen.getAllByRole('button');
+    const [, buttonAdd] = screen.getAllByRole("button");
 
     fireEvent.click(buttonAdd);
 
-    expect(screen.getByText('2')).toBeDefined();
-
-  })
-
-  test('should decrease count when -1 button is clicked', () => {
-    const quanity: number = 5;
-
-    render(<ItemCounter name="Item Counter test" quantity={quanity}/>);
-
-    const [buttonSubstract,] = screen.getAllByRole("button");
-
-    fireEvent.click(buttonSubstract);
-
-    expect(screen.getByText('4')).toBeDefined();
-
+    expect(screen.getByText("2")).toBeDefined();
   });
 
-  test('should not decrease when -1 button button is clicked and quantity is 1', () => {
-    const quanity:number = 1;
-    render(<ItemCounter name="Item Counter test" quantity={quanity}/>);
+  test("should decrease count when -1 button is clicked", () => {
+    const quanity: number = 5;
 
-    const [buttonSubstract,] = screen.getAllByRole('button');
+    render(<ItemCounter name="Item Counter test" quantity={quanity} />);
+
+    const [buttonSubstract] = screen.getAllByRole("button");
 
     fireEvent.click(buttonSubstract);
 
-    expect(screen.getByText('1')).toBeDefined();
+    expect(screen.getByText("4")).toBeDefined();
+  });
 
+  test("should not decrease when -1 button button is clicked and quantity is 1", () => {
+    const quanity: number = 1;
+    render(<ItemCounter name="Item Counter test" quantity={quanity} />);
+
+    const [buttonSubstract] = screen.getAllByRole("button");
+
+    fireEvent.click(buttonSubstract);
+
+    expect(screen.getByText("1")).toBeDefined();
+  });
+
+  test("should change to red color when counter is 1", () => {
+    const quanity: number = 1;
+    const name: string = "Test Item Couner";
+    render(<ItemCounter name={name} quantity={quanity} />);
+
+    const itemText = screen.getByText(name);
+
+    expect(itemText.style.color).toBe("red");
+  });
+
+  test("should change to black color when counter is greater than 1", () => {
+    const quanity: number = 2;
+    const name: string = "Test Item Couner";
+    render(<ItemCounter name={name} quantity={quanity} />);
+
+    const itemText = screen.getByText(name);
+
+    expect(itemText.style.color).toBe("black");
   });
 });
