5a618ee999b1ec7d36d3c10c952ec106e612587b
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-12-03 22:09:46 -0500

    Unitary tests created

--- first-steps/src/helpers/math.helper.test.ts
+++ first-steps/src/helpers/math.helper.test.ts
@@ -1,14 +1,80 @@
-import { expect, test } from "vitest"
-import { add } from "./math.helerper";
+import { describe, expect, test } from "vitest"
+import { add, multiply, substract } from "./math.helerper";
 
-test('should add two positives numbers', () => {
-    // 1. Arrange => Preparar
-    const a: number = 1;
-    const b: number = 2;
 
-    // 2. Act => Proporcionar estímulo
-    const result: number = add(a, b);
+describe('add', () => {
+    test('should add two positives numbers', () => {
+        // 1. Arrange => Preparar
+        const a: number = 1;
+        const b: number = 2;
 
-    // 3. Assert => Resultado esperado
-    expect(result).toBe(a + b);
+        // 2. Act => Proporcionar estímulo
+        const result: number = add(a, b);
+
+        // 3. Assert => Resultado esperado
+        expect(result).toBe(a + b);
+    });
+
+    test('should add two negatives numbers', () => {
+        const a: number = -3;
+        const b: number = -2;
+
+        const result: number = add(a, b);
+
+        expect(result).toBe(a + b);
+    });
 });
+
+describe('substract', () => {
+    test('should substract two positive numbers', () => {
+
+        const a: number = 4;
+        const b: number = 3;
+
+        const result = substract(a, b);
+
+        expect(result).toBe(a - b);
+
+    });
+
+    test('should substract two negative numbers', () => {
+
+        const a: number = -2;
+        const b: number = -3;
+
+        const result = substract(a, b);
+
+        expect(result).toBe(a - b);
+
+    });
+});
+
+describe('multiply', () => {
+    test('multiply two positive numbers', () => {
+        const a: number = 3;
+        const b: number = 2;
+
+        const result: number = multiply(a, b);
+
+        expect(result).toBe(a * b);
+    });
+
+    test('multiply one positive number by one negative number', () => {
+        const a: number = 3;
+        const b:number = -5;
+
+        const result = multiply(a, b);
+
+        expect(result).toBe(a * b);
+    });
+
+    test('multiply one positive number by zero (0)', () => {
+        const a: number = 2;
+        const b: number = 0;
+
+        const result = multiply(a, b);
+
+        expect(result).toBe(a * b);
+    })
+});
+
