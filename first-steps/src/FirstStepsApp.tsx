4f916869bbbf24a7cd23b2e87b5bcc529ddf13b2
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-12-03 21:08:23 -0500

    last changes

--- first-steps/src/FirstStepsApp.tsx
+++ first-steps/src/FirstStepsApp.tsx
@@ -1,9 +1,40 @@
+import { ItemCounter } from "./shopping-cart/ItemCounter";
+
+interface ItemInCart {
+  productName: string;
+  quantity: number;
+}
+
+const itemsInCart: ItemInCart[] = [
+  {
+    productName: "Play 5",
+    quantity: 2,
+  },
+  {
+    productName: "XBOX One",
+    quantity: 3,
+  },
+  {
+    productName: "Keyboard logitch",
+    quantity: 1,
+  },
+  {
+    productName: "Apple Ipords",
+    quantity: 3,
+  },
+  {
+    productName: "Samsung Smart TV",
+    quantity: 4,
+  },
+];
+
 export function FirstSetepsApp() {
   return (
     <div>
-      <h1>Hello World !!</h1>
-      <p>This is a paragraph</p>
-      <button>Click me</button>
+      <h1>Shopping Card</h1>
+      {itemsInCart.map(({ productName, quantity }) => (
+        <ItemCounter key={productName} name={productName} quantity={quantity} />
+      ))}
     </div>
   );
 }
--- first-steps/src/main.tsx
+++ first-steps/src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { FirstSetepsApp } from "./FirstStepsApp";
+// import { MyAwesomeApp } from "./MyAwesomeApp";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <FirstSetepsApp />
+    {/* <MyAwesomeApp /> */}
   </StrictMode>
 );
