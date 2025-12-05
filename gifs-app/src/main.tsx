213fe782e0620f733a318fcb25d2e194dd306356
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2025-12-04 21:38:16 -0500

    Gifs app initialized

---
+++ gifs-app/src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div>
+      <h1>
+        Hola Mundo
+      </h1>
+    </div>
+  </StrictMode>,
+)
